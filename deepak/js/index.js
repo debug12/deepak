f50d8f78bdb7df5aa65855392348a6732eff4382
Deepak <kumarde@umich.edu>
2013-08-03 13:08:38 -0400

    added lots of fun animations

--- deepak/js/index.js
+++ deepak/js/index.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
 	$(function(){
 		$('#typed').typed({
-			strings: ["First sentence.", "Second sentence."],
-			typeSpeed: 30
+			strings: ["Hi, I'm Deepak.", "I like to think, code, and sing.", "Lets get in touch."],
+			typeSpeed: 50,
+			backDelay: 400,
+			callback: function(){
+				$('.bash-outer').css('height: 100px')
+				$('#hideMe').fadeIn('slow');
+			}
 		})
 	})
 })
