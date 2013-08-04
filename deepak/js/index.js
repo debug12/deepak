338f233c638d5a4fd085694f25d9fbba304f519c
Deepak <kumarde@umich.edu>
2013-08-03 21:58:29 -0400

    need pics

--- deepak/js/index.js
+++ deepak/js/index.js
@@ -2,6 +2,11 @@ $(document).ready(function(){
 	var commands = [];
 	var alreadyUp = false;
 	var length = 0;
+	var hidden = 0;
+	var total = 3;
+	var secret1 = false;
+	var secret2 = false;
+	var secret3 = false;
 	$(function(){
 		$('#typed').typed({
 			strings: ["Hi, I'm Deepak.", "I like to think, code, and sing.", "Lets get in touch."],
@@ -21,7 +26,7 @@ $(document).ready(function(){
 		var keycode = (e.keyCode ? e.keyCode : e.which);
 		if(keycode == '13'){
 			//now program all the codes
-			var command = $(this).val();
+			var command = $(this).val().toLowerCase();
 			if(command == "blog"){
 				$('.clearMe').empty();
 				commands.push(command);
@@ -35,7 +40,7 @@ $(document).ready(function(){
 				$(this).attr('placeholder', 'type the page you want to see');
 				$('.clearMe').append("<li>blog</li><li>projects</li><li>music</li>")
 			}
-			else if(command == "projects"){
+			else if(command == "projects" || command == "hacks"){
 				$('.clearMe').empty();
 				$(this).val("");
 				commands.push(command);
@@ -48,19 +53,59 @@ $(document).ready(function(){
 			}
 			else if(command == ""){
 				$(this).val("");
-				$(this).attr('placeholder', 'type the page you want to see');
+				$(this).attr('placeholder', 'type help for usage');
 			}
 			else if(command == "help"){
 				$('.clearMe').empty();
 				commands.push(command);
 				$(this).val("");
 				$('.clearMe').append("<li>Use ls to display the pages</li><li>Type in those pages to navigate</li><li>Try and find all the hidden commands!</li>")
+				$('.clearMe').append("<li>Hint: Check the Jumbotron. </li>");
 			}
 			else if(command == "music"){
 				$('.clearMe').empty();
 				commands.push(command);
 				$(this).val("");
 				window.open('http://www.youtube.com/dpakkumar3216');
+			}
+			else if(command == "exit"){
+				$('.bash-outer').hide();
+			}
+			else if(command == "try and read this"){
+				commands.push(command);
+				$('.clearMe').empty();
+				$(this).val("");
+				if(!secret1){
+					secret1 = true;
+					hidden++;
+					$('.clearMe').append("<p>Congrats! You've found "+hidden+" secrets!</p>");
+					$('.clearMe').append("<p>You have "+(total-hidden)+" remaining.</p>");
+					if(hidden == total){
+						$('.clearMe').empty();
+						$('.clearMe').append("<p>Here's my number. (248) 231-3575</p>");
+						$('.clearMe').append("<p> Let's build something awesome. </p>");
+					}
+				} else{
+					$('.clearMe').append("<p>You've already found this one. </p>");
+				}
+			}
+			else if(command == "i see that you are catching on"){
+				$('.clearMe').empty();
+				commands.push(command);
+				$(this).val("");
+				if(!secret2){
+					secret2 = true;
+					hidden++;
+					$('.clearMe').append("<p>Congrats! You've found "+hidden+" secrets!</p>");
+					$('.clearMe').append("<p>You have "+(total-hidden)+" remaining.</p>");
+					if(hidden == total){
+						$('.clearMe').empty();
+						$('.clearMe').append("<p>Here's my number. (248) 231-3575</p>");
+						$('.clearMe').append("<p> Let's build something awesome. </p>");
+					}
+				} else{
+					$('.clearMe').append("<p>You've already found this one. </p>");
+				}
 			}
 			else{
 				$('.clearMe').empty();
@@ -86,13 +131,14 @@ $(document).ready(function(){
 		else if(e.keyCode == 40){
 			if(alreadyUp){
 				++length;
+				alert(commands);
 				$('.blackme').val(commands[length]);
 			}
 			return false;
 		}
 		else if(e.keycode == 9 || e.which == 9){
 			e.preventDefault();
-			var command = $(this).val();
+			var command = $(this).val().toLowerCase();
 			if(command == 'b' || command == 'bl' || command == 'blo' || command == 'blog'){
 				$(this).val('blog');
 			}
@@ -107,6 +153,9 @@ $(document).ready(function(){
 			}
 			else if(command == 'm' || command == 'mu' || command == 'mus' || command == 'musi' || command == 'music'){
 				$(this).val('music');
+			}
+			else if(command == 'e' || command == 'ex' || command == 'exi' || command == 'exit'){
+				$(this).val('exit');
 			}
 		}
 	})
